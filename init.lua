1e1287820741e4ebb4028a4cfd2ecc9f44bd7531
Libere Code <libere-code@posteo.org>
2026-04-26 00:37:26 +0200

    new mapping
    cursed floatingScratchBuf keybind

--- init.lua
+++ init.lua
@@ -3,6 +3,18 @@ local vim = vim
 
 vim.cmd("so " .. vim.fn.stdpath("config") .. "/file.vim") -- this works, ... after fix...
 
+vim.o.winborder = "+,-,+,|,+,-,+,|"
+
+vim.keymap.set("n", "<leader>s", function ()
+	vim.cmd([[let buf = nvim_create_buf(v:false, v:true)]])
+	vim.cmd([[call nvim_buf_set_lines(buf, 0, -1, v:true, ["test", "text"])]])
+	vim.cmd([[let opts = {'relative': 'cursor', 'width': 10, 'height': 2, 'col': 0,
+	\ 'row': 1, 'anchor': 'NW', 'style': 'minimal'}]])
+	vim.cmd([[let win = nvim_open_win(buf, 0, opts)]])
+	-- " optional: change highlight, otherwise Pmenu is used
+	vim.cmd([[call nvim_set_option_value('winhl', 'Normal:MyHighlight', {'win': win})]])
+end, { desc = '[s]cratch buffer'}) -- Cursed command D;
+
 local aucmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
